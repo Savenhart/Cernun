c4bd370f4af56c515056290f8e1c71c4547fdd45
Savenhart <theo.lounissi@gmail.com>
2020-01-23 14:07:20 +0100

    Add project modules

---
+++ simulife/models/src/test/java/fr/satysko/simulife/models/AppTest.java
@@ -0,0 +1,20 @@
+package fr.satysko.simulife.models;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
