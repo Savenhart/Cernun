d72fc4115001128537fe2997e9fc19b469a71b2e
Savenhart <theo.lounissi@gmail.com>
2020-02-27 16:42:10 +0100

    Logout/login button
    Add logout button and hide login/register button if connected

--- Cernun-GUI/src/app/components/header/header.component.ts
+++ Cernun-GUI/src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../_services/authentication.service';
+import { Router } from '@angular/router';
+import { User } from '../../_models/user.model';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  currentUser: User;
+  auth: AuthenticationService;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+}
 
   ngOnInit(): void {
   }
 
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+}
 }
