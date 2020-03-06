129b9abfcafb86e406c72f8d41893465e1eb4ce7
Savenhart <theo.lounissi@gmail.com>
2020-03-06 16:37:17 +0100

    Start IA and model
    Start the IA and implements food and creature model

--- ProofOfConcept/AI_Reinceforment_Learning/Creature.js
+++ ProofOfConcept/AI_Reinceforment_Learning/Creature.js
@@ -1,14 +1,12 @@
-export class Food{
-
+export class Creature{
+    
     constructor(x, y, color, ctx){
         this._x = x;
         this._y = y;
         this._color = color;
         this._ctx = ctx;
-        this._ctx.beginPath();
-        this._ctx.fillStyle = this._color;
-        this._ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
-        this._ctx.fill();
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x, this.y, 30, 30);
     }
 
     get x(){
@@ -43,14 +41,11 @@ export class Food{
         this._ctx = ctx;
     }
 
-    clearArc() {
-        this._ctx.save();
-        //this._ctx.globalCompositeOperation = 'destination-out';
-        this._ctx.clearRect(0,0,800, 600);
-        this._ctx.beginPath();
-        this._ctx.fillStyle = this._color;
-        this._ctx.arc(this._x, this._y, 5, 0, 2 * Math.PI);
-        this._ctx.fill();
-        this._ctx.restore();
+    clearRect() {
+        this.ctx.save();
+        this.ctx.clearRect(0,0,800, 600);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x, this.y, 30, 30);
+        this.ctx.restore();
       }
 }
